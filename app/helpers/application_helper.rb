441a721cb31edff3e0846922c7b5ec990884f441
Karina Almeida <s.karinaalmeida@gmail.com>
2022-06-27 22:33:55 -0300

    added method profile image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def profile_image(user, options = {size: 200})
+    email_address = user.email.downcase
+    size = options[:size]
+    hash = Digest::MD5.hexdigest(email_address)
+    gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+    image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block mt-3")
+  end
 end
