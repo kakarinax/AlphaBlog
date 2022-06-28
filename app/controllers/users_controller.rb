fbafd71fa5c6910f16a163355d527121d25b3d56
Karina Almeida <s.karinaalmeida@gmail.com>
2022-06-27 22:47:08 -0300

    updated update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,7 @@ class UsersController < ApplicationController
   def edit; end
 
   def update
+    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = 'User was update succesfully!'
       redirect_to @user
